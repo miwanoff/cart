83634af2c41d91d9141d16dcf45a8a36482c3ca2
miwanoff <marina.iva.noffa@gmail.com>
2021-01-23 16:26:48 +0200

    add addToCart

--- script.js
+++ script.js
@@ -9,3 +9,29 @@ function setCartData(o) {
 function getCartData() {
   return JSON.parse(localStorage.getItem("cart"));
 }
+
+// Добавляем товар в корзину
+function addToCart(e) {
+  let button = e.target;
+  button.disabled = true; // блокируем кнопку на время операции с корзиной
+  let cartData = getCartData() || {}; // получаем данные корзины или создаём новый объект, если данных еще нет
+  let parentBox = button.parentNode; // родительский элемент кнопки "Добавить в корзину";
+  let itemId = button.getAttribute("data-id"); // ID товара
+  let itemTitle = parentBox.querySelector(".item_title").innerHTML; // название товара
+  let itemPrice = parentBox.querySelector(".item_price").innerHTML; // стоимость товара
+  console.log(cartData);
+  if (cartData.hasOwnProperty(itemId)) {
+    // если такой товар уже в корзине, то добавляем +1 к его количеству
+    cartData[itemId][2] += 1;
+  } else {
+    // если товара в корзине еще нет, то добавляем в объект
+    cartData[itemId] = [itemTitle, itemPrice, 1];
+  }
+  // Обновляем данные в LocalStorage
+  setCartData(cartData);
+  button.disabled = false; // разблокируем кнопку после обновления
+  cartCont.innerHTML = "Товар добавлен в корзину.";
+  setTimeout(function () {
+    cartCont.innerHTML = "Продолжить покупки...";
+  }, 1000);
+}
