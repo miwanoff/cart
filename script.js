47382e8d061d54e6cbe31cfdda73c4d4bdd1ef8c
miwanoff <marina.iva.noffa@gmail.com>
2021-01-23 16:20:50 +0200

    add se/get to local storage

---
+++ script.js
@@ -0,0 +1,11 @@
+let itemBox = document.querySelectorAll(".item_box"); // блок каждого товара
+let cartCont = document.getElementById("cart_content"); // блок вывода данных корзины
+
+// Записываем данные в LocalStorage
+function setCartData(o) {
+  localStorage.setItem("cart", JSON.stringify(o));
+}
+// Получаем данные из LocalStorage
+function getCartData() {
+  return JSON.parse(localStorage.getItem("cart"));
+}
